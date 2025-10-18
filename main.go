439e85f0e1ea54912ec4a83318f46b408745dc0a
HussainWorld <164430022+HussainWorld@users.noreply.github.com>
2025-10-18 22:39:04 +0300

    hussain done

--- main.go
+++ main.go
@@ -1,24 +1,119 @@
 package main
 
 import (
+	"fmt"
 	"groupie-tracker/models"
-    "groupie-tracker/handlers"
-    "fmt"
-    "log"
-    "net/http"
-
-    
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
+var tmpl = template.Must(template.ParseFiles(
+	"templates/index.html",
+	"templates/artist.html",
+))
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	view := models.IndexData{
+		Artists: models.Data.Artists,
+	}
+	err := tmpl.ExecuteTemplate(w, "index.html", view)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func artistHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/artist/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil || id <= 0 {
+		http.NotFound(w, r)
+		return
+	}
+
+	var artist *models.Artist
+	for i := range models.Data.Artists {
+		if models.Data.Artists[i].ID == id {
+			artist = &models.Data.Artists[i]
+			break
+		}
+	}
+	if artist == nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	var location *models.Location
+	for i := range models.Data.Locations {
+		if models.Data.Locations[i].ID == id {
+			location = &models.Data.Locations[i]
+			break
+		}
+	}
+
+	var dates *models.Date
+	for i := range models.Data.Dates {
+		if models.Data.Dates[i].ID == id {
+			dates = &models.Data.Dates[i]
+			break
+		}
+	}
+
+	var relation *models.Relation
+	for i := range models.Data.Relations {
+		if models.Data.Relations[i].ID == id {
+			relation = &models.Data.Relations[i]
+			break
+		}
+	}
+
+	view := models.ArtistDetailData{
+		Artist:    *artist,
+		Relation:  models.Relation{},
+		Locations: nil,
+		Dates:     nil,
+	}
+
+	if location != nil {
+		view.Locations = location.Locations
+	}
+	if dates != nil {
+		view.Dates = dates.Dates
+	}
+	if relation != nil {
+		view.Relation = *relation
+	}
+
+	err = tmpl.ExecuteTemplate(w, "artist.html", view)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
-    handlers.InitTemplates()
+	// fetch data from the api
+	if err := models.LoadArtistsFromAPI(); err != nil {
+		log.Fatal(err)
+	}
+	if err := models.LoadDatesFromAPI(); err != nil {
+		log.Fatal("dates", err)
+	}
+	if err := models.LoadLocationFromAPI(); err != nil {
+		log.Fatal("locations", err)
+	}
+	if err := models.LoadRelationsFromAPI(); err != nil {
+		log.Fatal("relations", err)
+	}
 
-    if err := models.FetchAPIData(); err != nil {
-        log.Fatal("Error fetching API data:", err)
-    }
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-    handlers.RegisterRoutes()
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/artist/", artistHandler)
 
-    fmt.Println("Server started at http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- models/api.go
+++ models/api.go
@@ -1,92 +1,123 @@
 package models
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 const (
-    ArtistsAPI   = "https://groupietrackers.herokuapp.com/api/artists"
-    LocationsAPI = "https://groupietrackers.herokuapp.com/api/locations"
-    DatesAPI     = "https://groupietrackers.herokuapp.com/api/dates"
-    RelationAPI  = "https://groupietrackers.herokuapp.com/api/relation"
+	ArtistsAPI   = "https://groupietrackers.herokuapp.com/api/artists"
+	LocationsAPI = "https://groupietrackers.herokuapp.com/api/locations"
+	DatesAPI     = "https://groupietrackers.herokuapp.com/api/dates"
+	RelationAPI  = "https://groupietrackers.herokuapp.com/api/relation"
 )
 
-
-// FetchAPIData populates the global Data variable with API results
-func FetchAPIData() error {
-    client := &http.Client{Timeout: 10 * time.Second}
-
-    // Fetch artists
-    artists, err := fetchJSON[[]Artist](client, ArtistsAPI)
-    if err != nil {
-        return fmt.Errorf("error fetching artists: %w", err)
-    }
-    Data.Artists = artists
-
-    // Fetch locations
-    var locData struct {
-        Index []Location `json:"index"`
-    }
-    if err := fetchJSONStruct(client, LocationsAPI, &locData); err != nil {
-        return fmt.Errorf("error fetching locations: %w", err)
-    }
-    Data.Locations = locData.Index
-
-    // Fetch dates
-    var dateData struct {
-        Index []Date `json:"index"`
-    }
-    if err := fetchJSONStruct(client, DatesAPI, &dateData); err != nil {
-        return fmt.Errorf("error fetching dates: %w", err)
-    }
-    Data.Dates = dateData.Index
-
-    // Fetch relations
-    var relData struct {
-        Index []Relation `json:"index"`
-    }
-    if err := fetchJSONStruct(client, RelationAPI, &relData); err != nil {
-        return fmt.Errorf("error fetching relations: %w", err)
-    }
-    Data.Relations = relData.Index
-
-    return nil
+type LocationsResponse struct {
+	Index []Location `json:"index"`
+}
+type RelationsResponse struct {
+	Index []Relation `json:"index"`
+}
+type DatesResponse struct {
+	Index []Date `json:"index"`
 }
 
-// Generic JSON fetcher for typed responses
-func fetchJSON[T any](client *http.Client, url string) (T, error) {
-    var result T
-    resp, err := client.Get(url)
-    if err != nil {
-        return result, err
-    }
-    defer resp.Body.Close()
+func LoadLocationFromAPI() error {
+	resp, err := http.Get(LocationsAPI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return result, fmt.Errorf("API returned status: %d", resp.StatusCode)
-    }
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
 
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        return result, err
-    }
+	// Create a wrapper struct to match the JSON
+	var response LocationsResponse
 
-    return result, nil
+	// Decode into the wrapper
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return fmt.Errorf("failed to decode locations: %w", err)
+	}
+
+	// Assign the slice from the wrapper to the global locations variable
+	Data.Locations = response.Index
+	// fmt.Println(Data.Locations)
+	return nil
 }
 
-// JSON fetcher for custom struct targets
-func fetchJSONStruct(client *http.Client, url string, v interface{}) error {
-    resp, err := client.Get(url)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+func LoadRelationsFromAPI() error {
+	resp, err := http.Get(RelationAPI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return fmt.Errorf("API returned status: %d", resp.StatusCode)
-    }
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
 
-    return json.NewDecoder(resp.Body).Decode(v)
+	// Create a wrapper struct to match the JSON
+	var response RelationsResponse
+
+	// Decode into the wrapper
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return fmt.Errorf("failed to decode locations: %w", err)
+	}
+
+	// Assign the slice from the wrapper to the global locations variable
+	Data.Relations = response.Index
+	// fmt.Println(Data.Locations)
+	return nil
+}
+
+func LoadDatesFromAPI() error {
+	resp, err := http.Get(DatesAPI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// Create a wrapper struct to match the JSON
+	var response DatesResponse
+
+	// Decode into the wrapper
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return fmt.Errorf("failed to decode locations: %w", err)
+	}
+
+	// Assign the slice from the wrapper to the global locations variable
+	Data.Dates = response.Index
+	// fmt.Println(Data.Locations)
+	return nil
+}
+
+func LoadArtistsFromAPI() error {
+	resp, err := http.Get(ArtistsAPI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// Create a wrapper struct to match the JSON
+	var response []Artist
+
+	// Decode into the wrapper
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return fmt.Errorf("failed to decode locations: %w", err)
+	}
+
+	// Assign the slice from the wrapper to the global locations variable
+	Data.Artists = response
+	return nil
 }
